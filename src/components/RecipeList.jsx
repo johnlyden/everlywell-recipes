7d5b55f5b25d0c221cee15c487abf96b822156cd
johnlyden <johnlydennc@gmail.com>
2018-01-27 18:42:32 -0600

    fleshed out recipeList - mapping over recipes and rendering Recipes

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import Recipe from './Recipe';
 
 export default class RecipeList extends Component {
   constructor(props) {
     super(props);
+  }
+
+  renderRecipes() {
+    if (!this.props.recipes.length) { return <h2>Sorry we didn't find any matches</h2> };
+    return this.props.recipes.map( (recipe, index) => {
+      return <Recipe key={index}
+                     recipe={recipe} 
+                     onRecipeSelect={this.props.onRecipeSelect}/> 
+    });
+  }
+
+  render() {
+    return (
+      <div className="recipe-list-container">
+        <ul className="recipe-list">
+          {this.renderRecipes()}
+        </ul>
+      </div>
+    )
   }
 }
