4b7af1cae3f3cb53f4f9c7d098876ebb64ca88ad
johnlyden <johnlydennc@gmail.com>
2018-01-27 18:49:07 -0600

    fleshed out search bar modal - toggling closed - successfully making request with search term

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
 
-
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.state = { term: '', isOpen: false };
   }
 
@@ -14,12 +14,39 @@ export default class SearchBar extends Component {
     Modal.setAppElement('body');
   }
 
+  onInputChange(event) {
+    this.setState({term: event.target.value})
+  }
 
+  toggleSearch() {
+    const currentState = this.state.isOpen;
+    this.setState({ isOpen: !currentState });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.requestRecipes(this.state.term);
+    this.setState({ term: '' });
+    this.toggleSearch();
+  }
 
   render() {
     return (
       <div className="search-container">
-        
+        <div className="search-trigger" onClick={() => { this.toggleSearch() }}>
+          <i className="fa fa-2x fa-search" aria-hidden="true"></i>
+        </div>
+        <Modal isOpen={ this.state.isOpen } className="search-modal">
+          <div className="search-nav">
+            <button onClick={ () => this.toggleSearch() }>
+              <i className="fa fa-3x fa-arrow-circle-left" aria-hidden="true"></i>
+            </button>
+            <form onSubmit={this.onFormSubmit} >
+              <input type="text" value={this.state.term} onChange={this.onInputChange}></input>
+              <button type="submit"><i className="fa fa-2x fa-search" aria-hidden="true"></i></button>
+            </form>
+          </div>
+        </Modal>
       </div>
     )
   }
